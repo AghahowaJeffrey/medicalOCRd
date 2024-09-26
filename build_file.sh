63a118ae4ad72131618a85b93b6ca944f47b6b2d
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-09-25 22:12:15 -0700

    Update build_file.sh

--- build_file.sh
+++ build_file.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-echo "Installing pipenv"
-python3 -m pip install pipenv
+echo "Building project packages"
+python3 -m pip install -r requirements.txt
 
 echo "Building project packages..."
 python3 -m pipenv install --system --deploy --ignore-pipfile
-
-echo "Migrating Database..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
 
 echo "Creating dist folder..."
 mkdir -p dist
